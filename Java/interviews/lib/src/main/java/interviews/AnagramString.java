da97d43eb4c5275a50cf03029c6a90591757de29
hashem98 <hashemsmadi98@gmail.com>
2022-06-07 13:15:32 +0300

    inerview

---
+++ Java/interviews/lib/src/main/java/interviews/AnagramString.java
@@ -0,0 +1,27 @@
+package interviews;
+
+import java.util.Arrays;
+
+public class AnagramString {
+    public boolean isAnagram(String str1, String str2) {
+        String s1 = str1.replaceAll(" ", "");
+        String s2 = str2.replaceAll(" ", "");
+        boolean status = true;
+        if (s1.length() != s2.length()) {
+            status = false;
+        } else {
+            char[] ArrayS1 = s1.toLowerCase().toCharArray();
+            char[] ArrayS2 = s2.toLowerCase().toCharArray();
+            Arrays.sort(ArrayS1);
+            Arrays.sort(ArrayS2);
+            status = Arrays.equals(ArrayS1, ArrayS2);
+        }
+        if (status) {
+            System.out.println(s1 + " and " + s2 + " are anagrams");
+        } else {
+            System.out.println(s1 + " and " + s2 + " are not anagrams");
+        }
+        return status;
+    }
+
+}
